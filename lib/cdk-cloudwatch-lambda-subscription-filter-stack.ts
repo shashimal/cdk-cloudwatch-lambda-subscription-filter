63f2c8e88ea62cd809968bca64727fafe414d213
Duleendra Shashimal <duleendra@bambu.co>
2022-02-09 19:02:02 +0800

    Initial commit

---
+++ lib/cdk-cloudwatch-lambda-subscription-filter-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkCloudwatchLambdaSubscriptionFilterStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkCloudwatchLambdaSubscriptionFilterQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
